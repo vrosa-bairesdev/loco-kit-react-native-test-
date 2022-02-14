85210cc182a489d0d7c66829c5f95ad438fb39ed
Vin Rosa <vin.rosa@bairesdev.com>
2022-02-14 10:53:39 -0400

    Testing iOS

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, NativeEventEmitter, ScrollView } from 'react-native';
+import LocoKitModule from 'react-native-loco-kit';
 
-export default function App() {
+export default class App extends Component {
+  state = {
+    status: 'starting',
+    locationStatus: '...',
+    activity: '...',
+    item: '...',
+  };
+  componentDidMount() {
+    const bus = new NativeEventEmitter(LocoKitModule)
+    bus.addListener('LocationStatusEvent', (data) => this.setState({ locationStatus: data }))
+    bus.addListener('TimeLineStatusEvent', (data) => {this.setState({ item: data }); console.log(data); })
+    bus.addListener('ActivityTypeEvent', (data) => this.setState({ activity: data }))
+    LocoKitModule.isAvailable((available) => {
+      console.log(available)
+      if (available) {
+        LocoKitModule.setup("<API Key Goes Here>", (result) => {
+          this.setState({ status: result })
+          LocoKitModule.start()
+        });
+      } else {
+        this.setState({ status: "LocoKit not available" })
+      }
+    })
+  }
+  render(){
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.container}>STATUS: {this.state.status}</Text>
+      <Text style={styles.container}>Location Status: {this.state.locationStatus}</Text>
+      <Text style={styles.container}>Activity: {this.state.activity}</Text>
+      <ScrollView>
+        <Text style={styles.container}>TimelineItem: {this.state.item}</Text>
+      </ScrollView>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
